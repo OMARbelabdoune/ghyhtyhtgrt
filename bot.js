92b607c3f8a0bc216953ce6c01d86100acb7418d
OMARbelabdoune <41428371+OMARbelabdoune@users.noreply.github.com>
2018-07-25 22:15:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -191,6 +191,30 @@ client.on('message', message => {
        
 });
 
+client.on('message', msg => {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith(prefix)) return;
+  let command = msg.content.split(" ")[0];
+  command = command.slice(prefix.length);
+  let args = msg.content.split(" ").slice(1);
+
+    if(command === "clear") {
+        const emoji = client.emojis.find("name", "wastebasket")
+    let textxt = args.slice(0).join("");
+    if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+    if (textxt == "") {
+        msg.delete().then
+    msg.channel.send("```ضع عدد الرسائل التي تريد مسحها ```").then(m => m.delete(3000));
+} else {
+    msg.delete().then
+    msg.delete().then
+    msg.channel.bulkDelete(textxt);
+        msg.channel.send("```عدد الرسائل التي تم مسحها: " + textxt + "```").then(m => m.delete(3000));
+        }    
+    }
+}
+});
+
 const codes = {
     ' ': '   ',
     '0': '0⃣',
