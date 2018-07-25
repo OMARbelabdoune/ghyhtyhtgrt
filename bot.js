e9d97f95ddd243fd88f49055319bc7d6988de611
OMARbelabdoune <41428371+OMARbelabdoune@users.noreply.github.com>
2018-07-25 21:54:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -165,7 +165,6 @@ if (message.content.startsWith("f!cv")) {
 });
 
 client.on('message', message => {
-var prefix = "=";
        if(message.content === prefix + "mutechannel") {
                            if(!message.channel.guild) return message.reply('** This command only for servers**');
 
@@ -339,7 +338,6 @@ const Za7f = [
 });
 
 
-  var prefix = "f!";
 var rebel = ["https://f.top4top.net/p_682it2tg6.png","https://e.top4top.net/p_682a1cus5.png","https://d.top4top.net/p_682pycol4.png","https://c.top4top.net/p_682vqehy3.png","https://b.top4top.net/p_682mlf9d2.png","https://a.top4top.net/p_6827dule1.png","https://b.top4top.net/p_682g1meb10.png","https://a.top4top.net/p_682jgp4v9.png","https://f.top4top.net/p_682d4joq8.png","https://e.top4top.net/p_6828o0e47.png","https://d.top4top.net/p_6824x7sy6.png","https://c.top4top.net/p_682gzo2l5.png","https://b.top4top.net/p_68295qg04.png","https://a.top4top.net/p_682zrz6h3.png","https://f.top4top.net/p_6828vkzc2.png","https://e.top4top.net/p_682i8tb11.png"]
     client.on('message', message => {
         var args = message.content.split(" ").slice(1);
@@ -532,7 +530,6 @@ const codes = {
   
   
   client.on('message' , async message => {
-	  var prefix = "=";
          if(message.content.startsWith(prefix + "emoji")) {
             let args = message.content.split(" ").slice(1);
     if (args.length < 1) {
